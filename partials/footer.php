e949d49f390a67133942260bd2d88ec4db94b8a6
fabvoj <fvojar@gmail.com>
2024-03-12 18:45:14 +0100

    js na main kompe

--- partials/footer.php
+++ partials/footer.php
@@ -1,6 +1,7 @@
 </main>
 </div>
 <script src="../databazy/scripts/script.js"></script>
+<script src="../tono/scripts/script.js"></script>
 <script src="../scripts/script.js"></script>
 </body>
 </html>
